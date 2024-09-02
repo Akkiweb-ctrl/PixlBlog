186f507eabc3ca4ebca3e3a0f8af0b9b56f133f7
Akkiweb-ctrl <rananawareakash001@gmail.com>
2024-09-02 23:15:57 +0530

    json

--- api/index.js
+++ api/index.js
@@ -59,17 +59,18 @@ app.use(cors(corsOptions));
 //   &&
 //       allowMethods.includes(req.headers["access-control-request-method"]) &&
 //       allowHeaders.includes(req.headers["access-control-request-headers"])
-app.options("*", (req, res) => {
-    console.log("preflight");
-    if (
-        req.headers.origin === "https://pixl-blog-one.vercel.app"
-    ) {
-        console.log("pass");
-        res.status(200).json();
-    } else {
-        console.log("fail");
-    }
-});
+// app.options("*", (req, res) => {
+//     console.log("preflight");
+//     if (
+//         req.headers.origin === "https://pixl-blog-one.vercel.app"
+//     ) {
+//         console.log("pass");
+//         res.status(200).json();
+//     } else {
+//         console.log("fail");
+//     }
+// });
+app.options("*",cors(corsOptions));
 
 app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser())
