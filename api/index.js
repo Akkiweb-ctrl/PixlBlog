b6bc2e4bbb593a1e0dd4c072927d3bb0c705f19f
Akkiweb-ctrl <rananawareakash001@gmail.com>
2024-09-02 22:29:11 +0530

    json

--- api/index.js
+++ api/index.js
@@ -24,34 +24,34 @@ const connect = async () => {
 
 connect();
 const allowedOrigin = 'http://localhost:3000'
-// const corsOptions = {
-//     origin:["https://pixl-blog-one.vercel.app/"], //included origin as true
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true, //included credentials as true
-// };
+const corsOptions = {
+    origin : 'https://pixl-blog-one.vercel.app/', //included origin as true
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, //included credentials as true
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://pixl-blog-one.vercel.app"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-    res.setHeader("Access-Control-Max-Age", 7200);
+// app.use((req, res, next) => {
+//     res.setHeader(
+//       "Access-Control-Allow-Origin",
+//       "https://pixl-blog-one.vercel.app"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+//     );
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     res.setHeader("Access-Control-Allow-Private-Network", true);
+//     //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+//     res.setHeader("Access-Control-Max-Age", 7200);
   
-    next();
-  });
+//     next();
+//   });
 
   // Set preflight
 app.options("*", (req, res) => {
