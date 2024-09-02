c37bfa193f87045c761d32b173f55d537511fef3
Akkiweb-ctrl <rananawareakash001@gmail.com>
2024-09-02 23:17:36 +0530

    json

--- api/index.js
+++ api/index.js
@@ -70,7 +70,7 @@ app.use(cors(corsOptions));
 //         console.log("fail");
 //     }
 // });
-app.options("*",cors(corsOptions));
+// app.options("*",cors(corsOptions));
 
 app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser())
